27790e8a1b13d5c37b5a43dcee7573de742e3cb6
Nate Reiners <nate.reiners@gmail.com>
2016-12-12 10:57:37 -0800

    add user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,18 +5,46 @@ RSpec.describe User, type: :model do
     subject(:user) do
       FactoryGirl.build(:user,
         username: "Boris",
-        password: "password" )
+        password: "password")
     end
 
     it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
     it { should validate_presence_of(:password_digest) }
-    it { should validate_presence_of(:session_token) }
     it { should validate_length_of(:password).is_at_least(6) }
 
     it { should have_many(:goals) }
     it { should have_many(:comments) }
 
-    descibe ".find_by_credentials" do
+
+
+    describe "#is_password?" do
+      it "verifies password is correct" do
+        expect.user.is_password?("password").to be true
+      end
+
+      it "verifies a password is not correct" do
+        expect.user.is_password?("blahblahblah").to be false
+      end
+    end
+
+    describe "#reset_session_token!" do
+      it "resets the user's session token" do
+        user.valid?
+        old_session_token = user.session_token
+        user.reset_session_token!
+        expect(old_session_token).to_not eq(user.session_token)
+      end
+
+      it "returns the new session_token" do
+        expect(user.reset_session_token!).to eq(user.session_token)
+      end
+
+
+    end
+
+
+    describe ".find_by_credentials" do
       before { user.save! }
 
       it "returns user given good credentials" do
@@ -25,7 +53,7 @@ RSpec.describe User, type: :model do
 
       it "returns nil given bad credentials" do
         expect(User.find_by_credentials("Boris", "badpassword")).to eq(nil)
-      end 
+      end
     end
 
   end
