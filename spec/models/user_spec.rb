e66cd7c55c953a1e61df4a3711a3030a1e4e096a
Nate Reiners <nate.reiners@gmail.com>
2016-12-12 10:26:08 -0800

    add user validation tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe User do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
+    it { should have_many(:goals) }
+    it { should have_many(:comments) }
+
+  end
+end
