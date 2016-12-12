01049df81a10aee062a1d99120ff3719a03830cf
Nate Reiners <nate.reiners@gmail.com>
2016-12-12 12:15:42 -0800

    add some usersController methods to satisfy some specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :signed_in?
+
+  def sign_in(user)
+  end
+
+  def current_user
+  end
+
+  def signed_in?
+  end
+
+  def sign_out
+  end
+
+  def require_sign_in!
+  end
+  
 end
